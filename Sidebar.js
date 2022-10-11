5f3f4e4375556746676921df488fd730a0e252dc
yuneojin <104805105+yuneojin@users.noreply.github.com>
2022-10-11 14:18:22 +0900

    사이드바 헤더 다시 수정

--- Sidebar.js
+++ Sidebar.js
@@ -1,70 +1,60 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../styles/Sidebar.css';
 import SidebarItem from "./SidebarItem";
-import baekkyong from '../images/baekkyoung.jpg'
-import login from '../images/login.png'
-import mypage from '../images/mypage.png'
+import baekkyong from "../images/baekkyoung.jpg";
+import login from "../images/login.png";
+import mypage from "../images/mypage.png";
+import "../styles/Sidebar.css";
 
 function Sidebarheader() {
   return (
     <div id="sideheader">
-      <div id="logo">
-        WEB4
-      </div>
-    <div>
-      
-    <div id="clientbutton">
-      <div id="login">
-        <img id = "loginIcon" src={login}/>
-        <a href="/login">로그인</a>
-      </div>
-      
-      <div id="mypage">
-        <img id = "mypageIcon" src={mypage}/>
-        <a href="/mypage">마이페이지</a>
+      <div id="logo">WEB4</div>
+      <div id="clientbutton">
+        <div id="login">
+        <img id="loginIcon" src={login} />
+          <Link to="/sign-in">로그인</Link>
+        </div>
+        <div id="mypage">
+          <img id="mypageIcon" src={mypage} />
+          <Link to="/mypage">마이페이지</Link>
+        </div>
       </div>
     </div>
-
-    </div>
-  </div>
-    
-  )
+  );
 }
 
 function Baekkyong() {
   return (
     <div>
-      <img id="baekkyong" src={baekkyong}/>
+      <img id="baekkyong" src={baekkyong} />
     </div>
-  )
+  );
 }
 
 function Sidebar() {
-
   const menus = [
     { name: "한식", path: "/korean" },
     { name: "분식", path: "/school" },
     { name: "퓨전푸드", path: "/Fusion" },
-    { name: "상세정보", path: "/details" }
+    { name: "상세정보", path: "/details" },
   ];
 
   return (
-    <div className="sidebar">
-      <Sidebarheader></Sidebarheader>
-      {menus.map((menu, index) => {
-        return (
-          <div className="menu">
-          <Link to={menu.path} key={index}>
-            <SidebarItem
-              menu={menu}
-            />
-          </Link>
-          </div>
-        );
-      })}
+    <div className="Sidebar">
+      <div className="sidebar">
+        <Sidebarheader></Sidebarheader>
+        {menus.map((menu, index) => {
+          return (
+            <div className="menu">
+              <Link to={menu.path} key={index}>
+                <SidebarItem menu={menu} />
+              </Link>
+            </div>
+          );
+        })}
         <Baekkyong></Baekkyong>
-        
+      </div>
     </div>
   );
 }
